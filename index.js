9eb7fa2d4dedf8d19f774449bff9813fb42eafc3
Phetoho-Entle <phetomohohla1122@gmail.com>
2025-07-16 17:15:24 +0200

    Updated CSS and JS

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-function search (event) {
-    event.preventDefault();
-    let searchInputElement = document.querySelector("#city-input");
-    let city = searchInputElement.value;
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#city-input");
+  let city = searchInputElement.value;
 
-    let apiKey = "739f904148cfa8boc86t8a8b426aac3f";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
- 
-    axios.get(apiUrl).then(displayWeather);
+  let apiKey = "739f904148cfa8boc86t8a8b426aac3f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeather);
 }
 
-function displayWeather (response) {
-    let temperatureElement = document.querySelector("#current-temperature");
-    let temperature = Math.round(response.data.temperature.current);
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = response.data.city;
-    temperatureElement.innerHTML = `${temperature}°C`;
+function displayWeather(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  temperatureElement.innerHTML = `${temperature}°C`; 
+  
+let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  
 }
+
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
