b3447dfca09f0084914bdc7461561c1521a31d2c
Phetoho-Entle <phetomohohla1122@gmail.com>
2025-07-17 12:43:55 +0200

    Added dynamic weather data to project

--- index.js
+++ index.js
@@ -22,8 +22,37 @@ let cityElement = document.querySelector("#city");
   let iconElement = document.querySelector("#weather-icon");
   iconElement.setAttribute("src", response.data.condition.icon_url);
   iconElement.setAttribute("alt",response.data.condition.description);
+
+  let weatherConditionsElement = document.querySelector("#weather-conditions");
+  weatherConditionsElement.innerHTML = response.data.condition.description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+
+  let windSpeedElement = document.querySelector("#wind-speed");
+  windSpeedElement.innerHTML = `${Math.round(response.data.wind.speed)}km/h`;
+
+  let date = new Date(response.data.time * 1000);
+  let timeElement = document.querySelector("#time");
+  timeElement.innerHTML = formatDate(date);
 }
 
+function formatDate(date) {
+let minutes = date.getMinutes();
+let hours = date.getHours();
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+let day = days[date.getDay()];
+
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+} 
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+
+return `${day} ${hours}:${minutes} `;
+}
 
 function loadDefaultCity() {
   let city = "London";
